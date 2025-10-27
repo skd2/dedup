d34e896965aadb9a1615de80fa9410dfe9624a96
skd2 <srinivasd18@gmail.com>
2025-10-27 13:16:54 -0700

    Add doc blocks

--- dedup.php
+++ dedup.php
@@ -25,6 +25,9 @@ class Dedup {
 		exit(0);
 	}
 
+	/**
+	 * Runs basic checks if the script should run or not. And stores data into memory from the input file
+	 */
 	public function prelim_checks() {
 		if (!file_exists($this->leads_json_file_path)) {
 			fputs(STDERR, 'Error: Could not find leads file at: ' . $this->leads_json_file_path . PHP_EOL);
@@ -52,6 +55,11 @@ class Dedup {
 		}
 	}
 
+	/**
+	 * Main method to process the leads json structure's data
+	 * 
+	 * @return array $final The final result to be returned after deduplication process completes
+	 */
 	public function process_leads() {
 		$final = $this->leads_array;
 
@@ -68,6 +76,19 @@ class Dedup {
 		return $final;
 	}
 
+	/**
+	 * The helper method that actually checks for duplicate _id or email key values and removed the duplicate
+	 * value based on the older timestamp or the element that appears earlier if the timestamps are the same
+	 * 
+	 * @param string $id_type whether it is _id or email field to check against for duplicate values
+	 * @param string $id_value The associated value for the _id or email key
+	 * @param int $lead_index The numeric index value of the current element being processed
+	 * @param array $lead_element The array value of the current element being processed
+	 * @param array $temp_result The intermediate array to store the _id value or _email value to check for duplicates in subsequent calls
+	 * @param array $final The array that is initially a copy of the main $this->leads_array, and unneeded keys are undet, resulting in the final array as a response
+	 * 
+	 * @return array 
+	 */
 	private function dedup_helper($id_type, $id_value, $lead_index, $lead_element, $temp_result, $final) {
 		if (isset($temp_result[$id_value])) { // To check for duplicate _id or email value
 			if (strtotime($temp_result[$id_value]['entryDate']) <= strtotime($lead_element['entryDate'])) {
@@ -94,6 +115,14 @@ class Dedup {
 		return date('Y-m-d H:i:s T');
 	}
 
+	/**
+	 * Writes log messages to a file. Prefixes timestamp by default. Returns boolean if the Write to file succeeds
+	 * 
+	 * @param string $message String message to be written to the file
+	 * @param array $details Optional additional details to be appended to the log message
+	 * 
+	 * @return bool
+	 */
 	private function write_log($message, $details = []) {
 		if (!empty($details)) {
 			$details = var_export($details, true);
@@ -105,6 +134,7 @@ class Dedup {
 	}
 }
 
+// Start the dedup process
 $dedup = new Dedup($argv);
 $dedup->prelim_checks();
 $final = $dedup->process_leads();
